e5a2305779ed615c02c4df25f8d9874b0eaba561
Amir Mansoor <amirmansoor200@gmail.com>
2024-06-16 15:21:24 +0500

    delete post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,10 +1,36 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import Post from "../models/postModel.js";
 
+// create new post
 const createPost = asyncHandler(async (req, res) => {
   const { content } = req.body;
   const post = await Post.create({ user: req.user._id, content });
   res.json({ msg: "Post created successfully" });
 });
 
-export { createPost };
+// get single post using id
+const getSinglePost = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404);
+    throw new Error("No post found with the given id");
+  }
+});
+
+// delete your post
+const deletePost = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  if (post) {
+    await Post.deleteOne({ _id: post._id });
+    res.json({ msg: "Post deleted successfully" });
+  } else {
+    res.status(404);
+    throw new Error("No post found with the given id");
+  }
+});
+
+export { createPost, getSinglePost, deletePost };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { createPost } from "../controllers/postController.js";
+import {
+  createPost,
+  deletePost,
+  getSinglePost,
+} from "../controllers/postController.js";
 const router = express.Router();
 
 router.route("/create").post(protect, createPost);
+router.route("/getSinglePost/:id").get(protect, getSinglePost);
+router.route("/deletePost/:id").delete(protect, deletePost);
 export default router;
